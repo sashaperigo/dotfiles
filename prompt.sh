3acb1153742f0283bb7807b6defd01f9b6a989f2
Sasha Perigo <sasha.perigo@gmail.com>
2017-07-03 00:21:08 -0700

    Random kaomojis!

--- prompt.sh
+++ prompt.sh
@@ -1,12 +1,28 @@
 # Paste this code into your ~/.bash_profile to change your bash
-# terminal prompt. This prompt will display the branch you are on if
-# you're in a git repo.
+# terminal prompt. This prompt displays a random kaomoji as well
+# the branch you are on if you're in a git repo.
 #
 # Examples:
-# (´ ▽｀).。ｏ♡ folder
-# (´ ▽｀).。ｏ♡ git-repo [master]
+# ▽￣＼(￣▽￣)／￣▽  some_folder
+# (　^ (●●)－)＝☆ some_folder
+# (´ ▽｀).。ｏ♡ git_repo [master]
+# (｡・‧̫・｡).*＊♡  git_repo [staging]
 
-export PS1='\033[1;36m(´ ▽｀).。ｏ♡  \033[0m\W \033[0;32m$(git_branch)\033[0m\$ '
+export PS1='\033[1;33m$(random_kaomoji)  \033[0m\W \033[0;32m$(git_branch)\033[0m\$ '
+
+random_kaomoji() {
+    declare -a kaomoji=("(´ ▽｀).。ｏ♡"
+        "(｡･ω･｡)ﾉ♡"
+        "(｡・‧̫・｡).*＊♡"
+        "(◍•ᴗ•◍)♡ ✧*"
+        "°₊·ˈ∗♡( ˃̶᷇ ‧̫ ˂̶᷆ )♡∗ˈ‧₊°"
+        "(　^ (●●)－)＝☆"
+        "／人◕ ‿‿ ◕人＼"
+        "▽￣＼(￣▽￣)／￣▽"
+        "ヾ(＠^∇^＠)ノ")
+    index=$(( RANDOM % ${#kaomoji[@]} ))
+    echo ${kaomoji[$index]}
+}
 
 git_branch() { 
 	if [ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1
