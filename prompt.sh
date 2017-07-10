5cdac9b47c30a72cfa942a341c5f8d464f963ce0
Sasha Perigo <sasha.perigo@gmail.com>
2017-07-09 20:01:04 -0700

    Wrapped zero char parts of the prompt in brackets

--- prompt.sh
+++ prompt.sh
@@ -8,7 +8,7 @@
 # (´ ▽｀).。ｏ♡ git_repo [master]
 # (｡・‧̫・｡).*＊♡  git_repo [staging]
 
-export PS1='\033[1;33m$(random_kaomoji)  \033[0m\W \033[0;32m$(git_branch)\033[0m\$ '
+export PS1='\[\033[1;33m\]$(random_kaomoji)  \[\033[0m\]\W \[\033[0;32m\]$(git_branch)\[\033[0m\]\$ '
 
 random_kaomoji() {
     declare -a kaomoji=("(´ ▽｀).。ｏ♡"
